35f3337d3330ed587716b37c14276a3fa3bfdfa4
Polumatisrilakshmi <102572470+Polumatisrilakshmi@users.noreply.github.com>
2022-06-22 18:41:16 +0530

    Delete 15site_properties.js

--- Section2/15site_properties.js
+++
@@ -1,4 +0,0 @@
-const site_properties = {
-    home_page: "C:\\Users\\user\\Desktop\\LFP_Batch\\Final_Workshop\\addressBookHome.html",
-    add_contacts_page: "C:\\Users\\user\\Desktop\\LFP_Batch\\Final_Workshop\\addresBookForm.html"
-  };
